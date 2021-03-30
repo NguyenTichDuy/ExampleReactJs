d096bc8ff8e0ac625e8b492465b6b9c170924151
nguyentichduy <nguyentichduy10a15cmg@gmail.com>
2021-03-30 16:17:27 +0700

    add app.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./styles.css";
 import "./components/test";
 import Clock from "./components/clock";
 import LogginControll from "./components/LoginControll";
+import Calculator from "./components/Caculator";
+import Test from "./components/test";
 export default function App() {
   return (
     <div className="App">
-      <LogginControll />
+      <Test />
     </div>
   );
 }
